b24051f09cc0578fed8d54cc4878911c1430079d
Evgeny Safronov <division494@gmail.com>
2016-04-25 01:35:04 +0300

    chore(docs): add a planning note

--- src/lib.rs
+++ src/lib.rs
@@ -33,24 +33,30 @@ use CLOSED_BRACE;
 
 #[pub]
 expression -> Vec<Token>
-	= (format / text)+
+    = (format / text)+
 text -> Token
-	= result:text_char+ { Token::Literal(result.into_iter().collect()) }
+    = result:text_char+ { Token::Literal(result.into_iter().collect()) }
 text_char -> char
-	= "{{" { OPENED_BRACE }
-	/ "}}" { CLOSED_BRACE }
-	/ [^{}] { match_str.chars().next().unwrap() }
+    = "{{" { OPENED_BRACE }
+    / "}}" { CLOSED_BRACE }
+    / [^{}] { match_str.chars().next().unwrap() }
 format -> Token
     = "{" name:(name ++ ".") "}" { Token::Placeholder(name) }
 name -> Key
-	= [0-9]+ { Key::Id(match_str.parse().unwrap()) }
-	/ [a-zA-Z][a-zA-Z0-9]* { Key::Name(match_str.into()) }
+    = [0-9]+ { Key::Id(match_str.parse().unwrap()) }
+    / [a-zA-Z][a-zA-Z0-9]* { Key::Name(match_str.into()) }
 "#);
+
+// TODO: Format spec.
+// TODO: Error cases.
+// TODO: Remove all unwraps().
+// TODO: Refactoring and symbol visibility.
+// TODO: Benchmarking.
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Key {
-	Id(usize),
-	Name(String),
+    Id(usize),
+    Name(String),
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -69,16 +75,16 @@ pub enum Error {
 }
 
 fn find<'r>(value: &'r Value, key: &Key) -> Option<&'r Value> {
-	match *key {
-		Key::Id(id) => {
-			if let &Value::Array(ref value) = value {
-				value.get(id)
-			} else {
-				None
-			}
-		}
-		Key::Name(ref name) => value.find(name),
-	}
+    match *key {
+        Key::Id(id) => {
+            if let &Value::Array(ref value) = value {
+                value.get(id)
+            } else {
+                None
+            }
+        }
+        Key::Name(ref name) => value.find(name),
+    }
 }
 
 impl Generator {
@@ -97,19 +103,18 @@ impl Generator {
             match *token {
                 Token::Literal(ref literal) => result.push_str(&literal[..]),
                 Token::Placeholder(ref format) => {
-					let mut current = value;
-					for key in format {
-						match find(&current, key) {
-							Some(value) => { current = value; }
-							None => unimplemented!(),
-						}
-					}
+                    let mut current = value;
+                    for key in format {
+                        match find(&current, key) {
+                            Some(value) => { current = value; }
+                            None => unimplemented!(),
+                        }
+                    }
 
-					 result.push_str(&serde_json::to_string(&current).unwrap()[..]);
-				}
+                    result.push_str(&serde_json::to_string(&current).unwrap()[..]);
+                }
             }
         }
-
         Ok(result)
     }
 }
@@ -146,61 +151,61 @@ mod test {
     fn placeholder_ast() {
         let generator = Generator::new("{hello}").unwrap();
 
-		let expected = vec![Token::Placeholder(vec![Key::Name("hello".into())])];
+        let expected = vec![Token::Placeholder(vec![Key::Name("hello".into())])];
         assert_eq!(expected, generator.tokens);
     }
 
-	#[test]
-	fn literal_squash() {
-		let g = Generator::new("{{surrounded}}").unwrap();
+    #[test]
+    fn literal_squash() {
+        let g = Generator::new("{{surrounded}}").unwrap();
 
-		assert_eq!("{surrounded}", g.consume(&Value::Null).unwrap());
-	}
+        assert_eq!("{surrounded}", g.consume(&Value::Null).unwrap());
+    }
 
-	#[test]
-	fn literal_squash_beatle() {
-		assert_eq!("}{", &Generator::new("}}{{").unwrap().consume(&Value::Null).unwrap());
-	}
+    #[test]
+    fn literal_squash_beatle() {
+        assert_eq!("}{", &Generator::new("}}{{").unwrap().consume(&Value::Null).unwrap());
+    }
 
-	#[test]
-	fn literal_with_placeholder() {
-		let g = Generator::new("le message: {id}!").unwrap();
+    #[test]
+    fn literal_with_placeholder() {
+        let g = Generator::new("le message: {id}!").unwrap();
 
-		let mut map = BTreeMap::new();
+        let mut map = BTreeMap::new();
         map.insert("id".into(), Value::I64(42));
 
         let value = Value::Object(map);
 
-		assert_eq!("le message: 42!", g.consume(&value).unwrap());
-	}
+        assert_eq!("le message: 42!", g.consume(&value).unwrap());
+    }
 
-	#[test]
-	fn literal_with_nested_placeholder() {
-		let g = Generator::new("le message: {key.nested}!").unwrap();
+    #[test]
+    fn literal_with_nested_placeholder() {
+        let g = Generator::new("le message: {key.nested}!").unwrap();
 
-		let mut map = BTreeMap::new();
-		let mut key = BTreeMap::new();
+        let mut map = BTreeMap::new();
+        let mut key = BTreeMap::new();
         key.insert("nested".into(), Value::I64(42));
-		map.insert("key".into(), Value::Object(key));
+        map.insert("key".into(), Value::Object(key));
 
         let value = Value::Object(map);
 
-		assert_eq!("le message: 42!", g.consume(&value).unwrap());
-	}
+        assert_eq!("le message: 42!", g.consume(&value).unwrap());
+    }
 
-	#[test]
-	fn literal_with_nested_placeholder_with_array() {
-		let g = Generator::new("le message: {0.1}!").unwrap();
+    #[test]
+    fn literal_with_nested_placeholder_with_array() {
+        let g = Generator::new("le message: {0.1}!").unwrap();
 
-		let mut nested = Vec::new();
-		nested.push(Value::I64(42));
+        let mut nested = Vec::new();
+        nested.push(Value::I64(42));
         nested.push(Value::String("value".into()));
 
-		let mut array = Vec::new();
-		array.push(Value::Array(nested));
+        let mut array = Vec::new();
+        array.push(Value::Array(nested));
 
         let value = Value::Array(array);
 
-		assert_eq!("le message: \"value\"!", g.consume(&value).unwrap());
-	}
+        assert_eq!("le message: \"value\"!", g.consume(&value).unwrap());
+    }
 }
